675c7b62c694484dfcae487a3a56c86a55bab2ba
Caitlin Moroney <57643150+caitmmoroney@users.noreply.github.com>
2021-08-07 18:24:37 -0400

    added line to print products

--- scratch_script.py
+++ scratch_script.py
@@ -1,8 +1,6 @@
 # Imports
 import os
-import requests
 from api import CoinbaseExchangeAuth, PublicCoinbaseAuth
-import pandas as pd
 
 # Environment variables
 api_key = os.environ.get("API_KEY")
@@ -11,21 +9,16 @@ api_passphrase = os.environ.get("API_PASSPHRASE")
 api_url = os.environ.get("API_URL")
 
 
-auth = CoinbaseExchangeAuth(api_key, api_secret, api_passphrase)
+auth = CoinbaseExchangeAuth(api_key, api_secret, api_passphrase, api_url)
 
 # Get accounts
-if api_url[-1] != '/':
-    api_url = api_url + '/'
-r = requests.get(api_url + 'accounts', auth=auth)
-# print(r.json())
-df = pd.DataFrame.from_dict(r.json())
-df = df[df.balance != '0.0000000000000000']
-df.reset_index(inplace=True, drop=True)
+df = auth.getaccounts()
 print(df)
 
 publicAPI = PublicCoinbaseAuth(api_url)
-print(f'The current price of Ethereum is: {publicAPI.getprice()}')
-# {'message': 'NotFound'}
+print(f'The current price of Ethereum is: {publicAPI.getexchangeprice()}')
+
+print(f'Products available on Coinbase Pro: {publicAPI.getproducts()}')
 
 
 # # Place an order
